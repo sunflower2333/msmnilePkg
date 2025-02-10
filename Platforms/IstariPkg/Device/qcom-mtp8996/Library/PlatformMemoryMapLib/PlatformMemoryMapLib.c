0558fa77205a9e7bea7d53f8dd2ea90b61c21215
Gustave Monce <gustave.monce@outlook.com>
2025-02-10 21:32:16 +0100

    TMP

--- Platforms/IstariPkg/Device/qcom-mtp8996/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Platforms/IstariPkg/Device/qcom-mtp8996/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -32,10 +32,10 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     {"Reserved Cached 0", 0x80C50000, 0x000B0000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
     /* Conflict with new Display Reserved
     {"HLOS 2",            0x80D00000, 0x03300000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK}, */
-    {"HLOS 2",            0x80D00000, 0x02701000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+    {"HLOS 2",            0x80D00000, 0x02700000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
     /* Adjusted value from LK framebuffer setup, ideally we would want to move this using MDP5 registers */
     {"Display Reserved",  0x83400000, 0x00800000, AddMem, MEM_RES, WRITE_THROUGH, MaxMem, WRITE_THROUGH},
-    {"RAM Partition",     0x83C01000, 0x003FF000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+    {"RAM Partition",     0x83C00000, 0x00400000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
     {"HLOS 3",            0x84000000, 0x01800000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
     {"HYP",               0x85800000, 0x00600000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
     {"MPSS_EFS",          0x85E00000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
