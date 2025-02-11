c133cec0d32706b4e261e41053888885b2b2c235
Gustave Monce <gustave.monce@outlook.com>
2025-02-11 22:05:05 +0100

    Update PlatformMemoryMapLib.c

--- Platforms/IstariPkg/Device/qcom-mtp8996/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Platforms/IstariPkg/Device/qcom-mtp8996/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -50,8 +50,7 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     {"HLOS 4",            0x91B00000, 0x0E500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
     {"HLOS 5",            0xA0000000, 0xFFFFFFFFFFFFFFFF, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK}, */
     /* Moved due to crash issues, overlapped code issues (LK boot related?) */
-    {"DXE Heap",          0x91B00000, 0x03C00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
-    /* Something blocking between 0x95700000 and 0xA0000000 (Size: 0x0A900000) */
+    {"DXE Heap",          0x91B00000, 0x0E500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
     {"UEFI FD",           0xA0000000, 0x00300000, AddMem, SYS_MEM, SYS_MEM_CAP, BsCode, WRITE_BACK},
 
     /* RAM partition regions */
